5970e1e94dabee0e0d0b53c39032fb2c39717d06
younes hamdane <younes.hamdane11@gmail.com>
2019-07-01 15:44:08 +0100

    init commit : upload project

---
+++ src/main/java/com/yhamdane/cachequickstart/CachequickstartApplication.java
@@ -0,0 +1,13 @@
+package com.yhamdane.cachequickstart;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CachequickstartApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CachequickstartApplication.class, args);
+    }
+
+}
